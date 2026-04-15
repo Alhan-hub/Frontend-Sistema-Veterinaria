7a56220511af270b637ab8383f05a00794208b0e
Emanuel <emanuelatencia2407@gmail.com>
2026-04-15 18:21:35 -0500

    feat:crear métodos para consultar factura(s)

---
+++ web/src/app/core/services/factura.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+
+import { environment } from '../../../environments/environment';
+import { FacturaCreate, FacturaRead, FacturaUpdate } from '../../models/api.models';
+
+@Injectable({ providedIn: 'root' })
+export class FacturaService {
+  private readonly base = `${environment.apiUrl}/facturas`;
+
+  constructor(private readonly http: HttpClient) {}
+
+  list(): Observable<FacturaRead[]> {
+    const params = new HttpParams().set('skip', 0).set('limit', 500);
+    return this.http.get<FacturaRead[]>(`${this.base}`, { params });
+  }
+
+  get(id: string): Observable<FacturaRead> {
+    return this.http.get<FacturaRead>(`${this.base}/${id}`);
+  }
+
+}
