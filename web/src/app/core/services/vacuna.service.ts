4134414e008a3f108e1492c12746a5a373ac7e91
Emanuel <emanuelatencia2407@gmail.com>
2026-04-17 17:38:56 -0500

    feat:definir métodos para hacer peticiones al Back-end de vacuna

---
+++ web/src/app/core/services/vacuna.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+
+import { environment } from '../../../environments/environment';
+import { VacunaCreate, VacunaRead, VacunaUpdate } from '../../models/api.models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VacunaService {
+  private readonly base = `${environment.apiUrl}/vacunas`;
+
+  constructor(private readonly http: HttpClient) {}
+
+  list(): Observable<VacunaRead[]> {
+    // Mantengo el límite de 500 que tenías en facturas
+    const params = new HttpParams().set('skip', 0).set('limit', 500);
+    return this.http.get<VacunaRead[]>(`${this.base}`, { params });
+  }
+
+  get(id: Number): Observable<VacunaRead> {
+    return this.http.get<VacunaRead>(`${this.base}/${id}`);
+  }
+
+  create(body: VacunaCreate): Observable<VacunaRead> {
+    return this.http.post<VacunaRead>(`${this.base}`, body);
+  }
+
+  update(id: Number, body: VacunaUpdate): Observable<VacunaRead> {
+    return this.http.put<VacunaRead>(`${this.base}/${id}`, body);
+  }
+
+  delete(id: Number): Observable<void> {
+    return this.http.delete(`${this.base}/${id}`, { observe: 'response' })
+      .pipe(
+        map(() => undefined)
+      );
+  }
+}
