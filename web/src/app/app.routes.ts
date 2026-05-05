d0c96dc062524002ce025dbbc6a8a951f7254dd5
Emanuel <emanuelatencia2407@gmail.com>
2026-05-05 18:14:09 -0500

    feat:definir enrrutadores para los componentes de dominio

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -12,7 +12,40 @@ export const routes: Routes = [
     path: 'app',
     canActivate: [auditUserGuard],
     loadComponent: () => import('./features/shell/shell.component').then((m) => m.ShellComponent),
-   
+    children: [
+      { path: '', redirectTo: 'usuarios', pathMatch: 'full' },
+      {
+        path: 'usuarios',
+        loadComponent: () =>
+          import('./features/usuarios/usuario-list').then((m) => m.UsuarioListComponent),
+      },
+      {
+        path: 'propietarios',
+        loadComponent: () =>
+          import('./features/propietarios/propietario-list').then((m) => m.PropietarioListComponent),
+      },
+      {
+        path: 'mascotas',
+        loadComponent: () =>
+          import('./features/mascotas/mascota-list').then((m) => m.MascotaListComponent),
+      },
+      {
+        path: 'vacunas',
+        loadComponent: () =>
+          import('./features/vacunas/vacuna-list').then((m) => m.VacunaListComponent),
+      },
+      {
+        path: 'facturas',
+        loadComponent: () =>
+          import('./features/facturas/factura-list').then(
+            (m) => m.FacturaListComponent,
+          ),
+      },
+      {
+        path: 'citas',
+        loadComponent: () => import('./features/citas/cita-list').then((m) => m.CitaListComponent),
+      },
+    ],
   },
   { path: '**', redirectTo: 'login' },
 ];
