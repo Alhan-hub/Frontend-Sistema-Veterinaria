8aa2720c5da43f28908d53cd610c8a855a06391c
Emanuel <emanuelatencia2407@gmail.com>
2026-05-05 18:13:05 -0500

    feat:Defnir enrrutador por defecto y para login

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { auditUserGuard } from './core/audit-user.guard';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'login',
+    loadComponent: () => import('./features/login/login').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'app',
+    canActivate: [auditUserGuard],
+    loadComponent: () => import('./features/shell/shell.component').then((m) => m.ShellComponent),
+   
+  },
+  { path: '**', redirectTo: 'login' },
+];
